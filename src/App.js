448071a9a631b1335446bc4a894be4ee4379c06e
Anthony <anthonygibson24@gmail.com>
2022-05-07 01:18:16 -0500

    Recreated wire frame as. added header too

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>LA LA Land</h1>
+      </div>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+          <li>Learn React</li>
+          <li>Go to coffee shop</li>
+          <li>research how to properly use github</li>
+          <li>review DOM events, query selector</li>
+          <li>I find it interesting that the default viewport height is 100 on the App-header class in the App.css file</li>
+      </ul>
     </div>
   );
 }
